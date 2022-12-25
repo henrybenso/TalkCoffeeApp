97e061675dfec6286bafec47d1314c90a8edcc5f
henrybenso <henrykbenso1@gmail.com>
2022-12-25 03:13:27 -0800

    Initial Commit

---
+++ Coffee n Us/Coffee_n_UsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_n_UsApp.swift
+//  Coffee n Us
+//
+//  Created by henry benso on 12/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_n_UsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
